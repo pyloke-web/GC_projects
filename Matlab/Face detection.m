5c2a481db4f7aac80c6197c55ef4cba0234fe426
Loke Pak-Yen <84847880+pyloke-web@users.noreply.github.com>
2022-12-16 23:33:51 +0800

    Update Face detection.m

--- Matlab/Face detection.m
+++ Matlab/Face detection.m
@@ -1,1 +1,12 @@
-
+web = webcam();
+A = snapshot(web);
+alg = vision.CascadeObjectDetector();
+imshow(A);
+ 
+while true
+    A = snapshot(web);
+    A2 = rgb2gray(A);
+    bbox = step(alg,A2);
+    pic = insertObjectAnnotation(A,'rectangle',bbox,'Face');
+    imshow(pic);
+end
